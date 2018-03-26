2e676dbd08ae6483939dc6e88c3b3d6a983481d2
luhongbo <296177811@qq.com>
2018-03-26 22:10:48 +0800

    1、解决跨域请求问题

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Express-Mock
 Quickly build your own Mock server.
+
+若跨域，设置你的ajax请求地址为http://127.0.0.1:XXXX/xxx/xxx之类
--- server.js
+++ server.js
@@ -3,10 +3,18 @@ let Mock = require('mockjs');
 
 let app = express();
 
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
+
 app.all('/test.action', function(req, res) {
     res.json(Mock.mock({
-        "status": 200,
-        "data|1-9": [{
+        "data|70-120": [{
             "name|5-8": /[a-zA-Z]/,
             "id|+10": 1,
             "value|0-500": 20
@@ -14,4 +22,4 @@ app.all('/test.action', function(req, res) {
     }))
 });
 
-app.listen('8080');
+app.listen('8090');
